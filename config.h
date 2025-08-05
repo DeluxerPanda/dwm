df5324e4ead2b8a39bc27950a888d8c1f3687a46
DeluxerPanda <cedrikpersson@gmail.com>
2025-08-05 19:35:01 +0200

    fonts fix

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray             = 1;        /* 0 means no systray */
 static const int showbar                 = 1;        /* 0 means no bar */
 static const int topbar                  = 1;        /* 0 means bottom bar */
-static const char *fonts[]               = { "monospace:size=10" };
+static const char *fonts[]                = { "JetBrainsMonoNL Nerd Font Mono:size=16", "NotoColorEmoji:pixelsize=16:antialias=true:autohint=true"  };
 static const char col_white[]            = "#eceff4";
 static const char col_Cyan[]             = "#00FFFF";
 static const char col_Sel[]              = "#2e3440";
