ead96eb6168659e0d2b17197d2638f5b70f2f227
DeluxerPanda <85413771+DeluxerPanda@users.noreply.github.com>
2025-10-01 22:14:57 +0200

    Update setup_for_Arch.sh

--- setup_for_Arch.sh
+++ setup_for_Arch.sh
@@ -62,13 +62,13 @@ function Installing() {
     sudo pacman -Syu --noconfirm
 
 #system
-    sudo pacman -S --needed --noconfirm base-devel libx11 libxft xorg-server xorg-xinit ffmpeg networkmanager mate-polkit nfs-utils nano
+    sudo pacman -S --needed --noconfirm base-devel libx11 libxft xorg-server xorg-xinit ffmpeg networkmanager mate-polkit nfs-utils nano usbutils
 
 #fonts
     sudo pacman -S --needed --noconfirm ttf-jetbrains-mono-nerd noto-fonts-emoji
 
 #programs
-    sudo pacman -S --needed --noconfirm rofi arandr xarchiver mpv firefox pavucontrol feh pcmanfm-gtk3 dunst
+    sudo pacman -S --needed --noconfirm rofi arandr xarchiver mpv firefox pavucontrol feh pcmanfm-gtk3 dunst flameshot
  
 #KDE apps
     sudo pacman -S --needed --noconfirm kdeconnect
