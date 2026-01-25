24639a454e437eb1ad1c6090a71a022310e1ca60
DeluxerPanda <85413771+DeluxerPanda@users.noreply.github.com>
2026-01-25 20:04:38 +0100

    Enhance Artch-install.sh: Add Desktop Environment setup and essential packages

--- Artch-install.sh
+++ Artch-install.sh
@@ -616,6 +616,40 @@ echo "  Reflector enabled"
 
 echo -ne "
 -------------------------------------------------------------------------
+                    Dekstop Environment Setup and Essentials packages
+-------------------------------------------------------------------------
+"
+export work_dir="$(pwd)"
+sudo systemctl enable --now NetworkManager
+sudo pacman -Sy --noconfirm
+sudo pacman -Syu --noconfirm
+sudo pacman -S --needed --noconfirm kdeconnect starship bash-completion bat fastfetch btop pavucontrol mpv firefox feh flameshot wget plasma konsole kate dolphin ark nfs-utils nano usbutils gnome-keyring fuse ffmpeg flatpak steam
+sudo systemctl enable sddm
+
+    # Install YAY
+    git clone https://aur.archlinux.org/yay-bin.git
+    cd yay-bin
+    makepkg --noconfirm -si
+    cd "$work_dir"
+    rm -rf yay-bin
+
+    # Optional GoXLR support
+    if lsusb | grep -q "GoXLRMini"; then
+        yay -S --needed --noconfirm goxlr-utility
+    fi
+
+        # Config files
+    mkdir -p ~/.config
+    cp "$work_dir/config/starship.toml" ~/.config/
+    mkdir -p ~/.config/fastfetch && cp -r "$work_dir/config/fastfetch/" ~/.config/
+
+    # Shell configs
+    cp "$work_dir/config/.bashrc" ~/.bashrc
+    
+    cd "$work_dir"
+
+echo -ne "
+-------------------------------------------------------------------------
                     Cleaning
 -------------------------------------------------------------------------
 "
